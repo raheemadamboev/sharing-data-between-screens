5605112fe2c3c2f0b93a7b283f0f2f070444668c
raheemadamboev <vividricemaryandmax@gmail.com>
2023-07-16 19:47:14 +0500

    Implemented PersistentStorage;

--- app/src/main/java/xyz/teamgravity/sharingdatabetweenscreens/MainActivity.kt
+++ app/src/main/java/xyz/teamgravity/sharingdatabetweenscreens/MainActivity.kt
@@ -6,13 +6,14 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import xyz.teamgravity.sharingdatabetweenscreens.ui.theme.SharingDataBetweenScreensTheme
+import dagger.hilt.android.AndroidEntryPoint
+import xyz.teamgravity.sharingdatabetweenscreens.persistent_storage.PersistentStorage
+import xyz.teamgravity.sharingdatabetweenscreens.presentation.theme.SharingDataBetweenScreensTheme
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -21,6 +22,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
+                    PersistentStorage()
                 }
             }
         }
